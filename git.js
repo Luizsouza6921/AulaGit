b0edf9e83588b72aa7635407a60993e96a7bb4e8
Luiz Gustavo Cruz De Souza <luizgcs0204@hotmail.com>
2025-04-07 15:26:01 -0300

    Ordenação

--- git.js
+++ git.js
@@ -1,4 +1,5 @@
 let comidas = ['Lasanha', 'Pizza', 'Sushi', 'Hambúrguer',];
 console.log(`Lista de todas as comidas: ${comidas}`);
 console.log(`primeira comida: ${comidas[0]}`);
-
+comidas.sort();
+console.log(`lista de todas as comidas ordenada: ${comidas}`);
