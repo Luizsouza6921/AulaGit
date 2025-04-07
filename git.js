5ef232baacba96be7083ccef61e2a52688db2774
Luiz Gustavo Cruz De Souza <luizgcs0204@hotmail.com>
2025-04-07 15:48:51 -0300

    odernação

--- git.js
+++ git.js
@@ -3,3 +3,4 @@ console.log(`Lista de todas as comidas: ${comidas}`);
 console.log(`primeira comida: ${comidas[0]}`);
 comidas.sort();
 console.log(`lista de todas as comidas ordenada: ${comidas}`);
+console.log('FIM!');
